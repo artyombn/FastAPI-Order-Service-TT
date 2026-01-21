7a111b015b05d2b86b0761cc7e7b8d807e041dc7
ArtemB <balabashinan@gmail.com>
2026-01-21 23:24:30 +0300

    add Order-Product pydantic schema

---
+++ app/schemas/order_product.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, Field
+
+
+class OrderProductBase(BaseModel):
+    """
+    Base Order–Product schema
+    """
+
+    order_id: int = Field(description="Order_id")
+    product_id: int = Field(description="Product_id")
+
+    product_quantity: int = Field(
+        ge=1,
+        description="Product quantity in the order >= 1",
+    )
+    product_price_at_order: float = Field(
+        ge=0.0,
+        description="Product price at the moment of order >= 0.0",
+    )
+
+
+class OrderProductOutput(OrderProductBase):
+    """
+    Response model
+    """
+
+    model_config = {"from_attributes": True}
