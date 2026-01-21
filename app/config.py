200c9a36065092ce8c32d7fb609fec835ecbde3f
ArtemB <balabashinan@gmail.com>
2026-01-22 01:04:40 +0300

    Docker settings

--- app/config.py
+++ app/config.py
@@ -2,21 +2,32 @@ import os
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
     DB_USER: str
     DB_PASSWORD: str
     DB_HOST: str
     DB_PORT: int
     DB_NAME: str
+    DOCKER: bool = False
 
     model_config = SettingsConfigDict(
-        env_file=os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), ".env"),
-        extra="allow"
+        env_file=os.path.join(
+            os.path.dirname(os.path.dirname(os.path.abspath(__file__))), ".env"
+        ),
+        extra="allow",
     )
 
     def get_db_url(self):
-        return (f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@"
-                f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}")
+        if self.DOCKER:
+            return (
+                f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@"
+                f"pgdb:{self.DB_PORT}/{self.DB_NAME}"
+            )
+        return (
+            f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@"
+            f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        )
+
 
 settings = Settings()
-
