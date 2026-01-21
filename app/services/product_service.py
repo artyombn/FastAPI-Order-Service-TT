ea7c2bb2ec7c1c153b000d7f3484f36265d04487
ArtemB <balabashinan@gmail.com>
2026-01-22 00:30:34 +0300

    add Product services

---
+++ app/services/product_service.py
@@ -0,0 +1,17 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database.models import Product as Product_db
+
+
+class ProductServices:
+
+    @classmethod
+    async def find_one_or_none_by_id(
+        cls,
+        product_id: int,
+        session: AsyncSession,
+    ) -> Product_db | None:
+        query = select(Product_db).where(Product_db.id == product_id)
+        product = await session.execute(query)
+        return product.scalar_one_or_none()
